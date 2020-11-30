5ac8d6e6f054edf79d9d120d93aa988fcf375be8
mostafa-asefy <asefi.v@gmail.com>
2020-11-30 08:41:15 +0330

    L18 - ImageCreateForm

---
+++ hamru/images/forms.py
@@ -0,0 +1,39 @@
+from django import forms
+from .models import Image
+from django.utils.text import slugify
+from urllib import request
+from django.core.files.base import ContentFile
+
+
+class ImageCreateForm(forms.ModelForm):
+
+    class Meta:
+        model = Image
+        fields = ('title', 'url', 'description')
+        widgets = {
+            'url': forms.HiddenInput,
+        }
+
+    def clean_url(self):
+        url = self.cleaned_data['url']
+        valid_extensions = ['jpg', 'jpeg']
+        extension = url.rsplit('.', 1)[1].lower()
+        if extension not in valid_extensions:
+            raise forms.ValidationError('The given URL does not '
+                                        'match valid image extensions.')
+        return url
+
+    def save(self, force_insert=False, force_update=False, commit=True):
+        image = super().save(commit=False)
+        image_url = self.cleaned_data['url']
+        extension = image_url.rsplit('.', 1)[1].lower()
+        name = slugify(image.title)
+        image_name = f'{name}.{extension}'
+
+        #Download image
+        response = request.urlopen(image_url)
+        image.image.save(image_name, ContentFile(response.read()),
+                         commit=False)
+        if commit:
+            image.save()
+        return image
