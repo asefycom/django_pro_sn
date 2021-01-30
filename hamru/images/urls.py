834384eeff495096d917a542f8942c3cefa60f66
mostafa-asefy <asefi.v@gmail.com>
2021-01-30 09:04:21 +0330

    L26 - Like Images part 1

--- hamru/images/urls.py
+++ hamru/images/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('create/', views.image_create, name='create'),
     path('detail/<int:id>/<slug:slug>/',
          views.image_detail, name='detail'),
+    path('like/', views.image_like, name='like'),
 ]
--- hamru/images/views.py
+++ hamru/images/views.py
@@ -4,6 +4,8 @@ from .forms import ImageCreateForm
 from django.contrib import messages
 from .models import Image
 from django.shortcuts import get_object_or_404
+from django.views.decorators.http import require_POST
+from django.http import JsonResponse
 
 
 @login_required
@@ -28,3 +30,22 @@ def image_detail(request, id, slug):
                   'images/image/detail.html',
                   {'section': 'images',
                    'image': image})
+
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status': 'ok'})
+        except:
+            pass
+
+    return JsonResponse({'status': 'error'})
+
