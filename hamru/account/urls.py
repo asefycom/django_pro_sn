c932a8d10dada551e8480048517f30bfb4ac03ea
mostafa-asefy <asefi.v@gmail.com>
2021-02-27 21:09:05 +0330

    L37 - FollowA

--- hamru/account/urls.py
+++ hamru/account/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('edit/', views.edit, name='edit'),
     path('users/', views.user_list, name='user_list'),
+    path('users/follow/', views.user_follow, name='user_follow'),
     path('users/<username>/', views.user_detail, name='user_detail'),
-
 ]
--- hamru/account/views.py
+++ hamru/account/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
-from .models import Profile
+from .models import Profile, Contact
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
+
+from common.decorators import ajax_required
 
 
 def user_login(request):
@@ -90,3 +94,24 @@ def user_detail(request, username):
                   {'section': 'people',
                    'user': user})
 
+
+@ajax_required
+@login_required
+@require_POST
+def user_follow(request):
+    user_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+            if action == 'follow':
+                Contact.objects.get_or_create(user_from=request.user,
+                                              user_to=user)
+            else:
+                Contact.objects.filter(user_from=request.user,
+                                              user_to=user).delete()
+            return JsonResponse({'status': 'ok'})
+        except:
+            pass
+
+    return JsonResponse({'status': 'error'})
