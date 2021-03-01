e366f30e982d3205f5700fa266d5cd25e798271e
mostafa-asefy <asefi.v@gmail.com>
2021-03-01 08:18:59 +0330

    L40 - ContentTypes

--- hamru/actions/models.py
+++ hamru/actions/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 
+
 class Action(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              related_name='actions',
